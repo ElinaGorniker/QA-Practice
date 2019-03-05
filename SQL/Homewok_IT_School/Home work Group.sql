af245f6062f434b319bac35c9b38b872ce549b08
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2019-03-05 15:44:40 +0700

    Add files via upload

--- SQL/Homewok_IT_School/Home work Group.sql
+++ SQL/Homewok_IT_School/Home work Group.sql
@@ -1,23 +1,7 @@
-SELECT 
-	CountryCode AS Country, COUNT(*) AS Count_of_city, AVG(Population) 
-	FROM city
-    GROUP BY Country
-	ORDER BY AVG(Population) DESC;
+SELECT CountryCode AS Country, COUNT(*) AS Count_of_city, AVG(Population) FROM city GROUP BY Country ORDER BY AVG(Population) DESC;
 	
-SELECT 
-    CountryCode AS Country,  COUNT(*) AS Count_of_city
-	FROM city
-	GROUP BY CountryCode;
+SELECT CountryCode AS Country, COUNT(*) AS Count_of_city FROM city GROUP BY CountryCode;
 	
-SELECT 
-    CountryCode AS Country,  COUNT(*) AS Count_of_city
-	FROM city
-	GROUP BY CountryCode
-	HAVING COUNT(*) >= 2;
+SELECT CountryCode AS Country, COUNT(*) AS Count_of_city FROM city GROUP BY CountryCode HAVING COUNT(*) >= 2;
 	
-SELECT 
-    CountryCode AS Country,  COUNT(*) AS Count_of_city
-	FROM city
-	WHERE Population >= 1000000
-	GROUP BY CountryCode
-	HAVING COUNT(*) >= 2;
+SELECT CountryCode AS Country, COUNT(*) AS Count_of_city FROM city WHERE Population >= 1000000 GROUP BY CountryCode HAVING COUNT(*) >= 2;
--- SQL/Homewok_IT_School/Home work Select.sql
+++ SQL/Homewok_IT_School/Home work Select.sql
@@ -4,31 +4,21 @@ SELECT * FROM city;
 
 SELECT Name, District FROM city;
 
-SELECT * FROM city
-	WHERE  CountryCode = 'RUS'
-	ORDER BY Name;
+SELECT * FROM city WHERE CountryCode = 'RUS' ORDER BY Name;
 	
-SELECT CountryCode, District FROM city DISTINKT
-	ORDER BY CountryCode;
+SELECT CountryCode, District FROM city ORDER BY CountryCode;
 	
-SELECT * FROM city
-	WHERE CountryCode IN ('ESP' , 'GRC', 'PRT')
-	ORDER BY Name DESC;
+SELECT * FROM city WHERE CountryCode IN ('ESP' , 'GRC', 'PRT') ORDER BY Name DESC;
 	
-SELECT  Name, Population FROM city
-	WHERE Population > 300000 AND Population < 500000;
+SELECT  Name, Population FROM city WHERE Population >= 300000 AND Population <= 500000;
 
-SELECT Name FROM city
-	WHERE Name LIKE 'A%';
+SELECT Name FROM city WHERE Name LIKE 'A%';
     
-SELECT Name FROM city
-	WHERE Name LIKE '%A%';
+SELECT Name FROM city WHERE Name LIKE '%A%';
 	
-SELECT Name, CountryCode, Population FROM city    
-	WHERE CountryCode = 'RUS' AND Population > 1000000;
+SELECT Name, CountryCode, Population FROM city WHERE CountryCode = 'RUS' AND Population >= 1000000;
 	
-SELECT Name, CountryCode, Population FROM city    
-    WHERE (CountryCode = 'ESP' AND Name LIKE 'A%')
-		OR (CountryCode = 'GRC' AND Population < 200000);
+SELECT Name, CountryCode, Population FROM city WHERE (CountryCode = 'ESP' AND Name LIKE 'A%') 
+OR (CountryCode = 'GRC' AND Population < 200000);
         
         
