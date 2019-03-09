5fdc8f7f86b3c10c1c0b5c6bb42ae99b0dec495f
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2019-03-09 08:45:01 +0700

    Add files via upload

--- SQL/Homewok_IT_School/Home work Date.sql
+++ SQL/Homewok_IT_School/Home work Date.sql
@@ -1,6 +1,12 @@
-ALTER TABLE products ADD date_of_arrival DATE DEFAULT NULL;
+ALTER TABLE products 
+ADD date_of_arrival DATE DEFAULT NULL;
 
-SELECT id, name, price, categoryID, DAY(date_of_arrival) AS Day, MONTH(date_of_arrival) AS Month, YEAR(date_of_arrival) AS Year FROM products;
+SELECT id, name, price, categoryID, 
+DAY(date_of_arrival) AS Day, 
+MONTH(date_of_arrival) AS Month, 
+YEAR(date_of_arrival) AS Year 
+FROM products;
 
-SELECT YEAR(date_of_arrival) AS Year, COUNT(*) AS Count FROM products GROUP BY Year;
-
+SELECT YEAR(date_of_arrival), COUNT(*) AS Count 
+FROM products 
+GROUP BY YEAR(date_of_arrival);  
--- SQL/Homewok_IT_School/Home work Group.sql
+++ SQL/Homewok_IT_School/Home work Group.sql
@@ -1,7 +1,20 @@
-SELECT CountryCode AS Country, COUNT(*) AS Count_of_city, AVG(Population) FROM city GROUP BY Country ORDER BY AVG(Population) DESC;
+SELECT CountryCode, COUNT(*) AS Count_of_city, AVG(Population) 
+FROM city 
+GROUP BY CountryCode
+ORDER BY AVG(Population) DESC;
 	
-SELECT CountryCode AS Country, COUNT(*) AS Count_of_city FROM city GROUP BY CountryCode;
+SELECT CountryCode AS Country, COUNT(*) AS Count_of_city 
+FROM city 
+GROUP BY CountryCode;
 	
-SELECT CountryCode AS Country, COUNT(*) AS Count_of_city FROM city GROUP BY CountryCode HAVING COUNT(*) >= 2;
+SELECT CountryCode AS Country, COUNT(*) AS Count_of_city 
+FROM city 
+GROUP BY CountryCode 
+HAVING COUNT(*) >= 2;
 	
-SELECT CountryCode AS Country, COUNT(*) AS Count_of_city FROM city WHERE Population >= 1000000 GROUP BY CountryCode HAVING COUNT(*) >= 2;
+SELECT CountryCode AS Country, COUNT(*) AS Count_of_city 
+FROM city 
+WHERE Population >= 1000000 
+GROUP BY CountryCode 
+HAVING COUNT(*) >= 2;
+USE world
