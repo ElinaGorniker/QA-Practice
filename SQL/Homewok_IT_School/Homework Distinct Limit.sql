f397203566bad1139a4c1ef3a8473c7a447cdfe5
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2019-03-10 09:38:35 +0700

    Add files via upload

---
+++ SQL/Homewok_IT_School/Homework Distinct Limit.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT Continent FROM country
+WHERE SurfaceArea >= 950000; 
+
+SELECT Name FROM country 
+ORDER BY SurfaceArea DESC
+LIMIT 5;
+
+SELECT Name FROM country 
+ORDER BY SurfaceArea DESC
+LIMIT 5, 5;
---
+++ SQL/Homewok_IT_School/Homework Join.sql
@@ -0,0 +1,25 @@
+SELECT * FROM country 
+CROSS JOIN city;
+
+SELECT ctr.code, ctr.name, c.name 
+FROM country AS ctr 
+INNER JOIN city AS c 
+	ON ctr.capital = c.ID;
+
+SELECT c.name, c.Population, ctr.code, ctr.name 
+FROM country AS ctr 
+INNER JOIN city AS c 
+	ON ctr.code = c.countrycode;
+
+SELECT ctr.continent, COUNT(c.id) AS city_count 
+FROM country AS ctr 
+LEFT JOIN city AS c 
+	ON c.countrycode = ctr.code
+GROUP BY ctr.continent;
+
+SELECT ctr.name, COUNT(la.countrycode) AS lang_count 
+FROM country AS ctr 
+LEFT JOIN countrylanguage AS la
+	ON ctr.code = la.countrycode AND la.isofficial = 'T'
+GROUP BY la.countrycode
+ORDER BY lang_count DESC;
