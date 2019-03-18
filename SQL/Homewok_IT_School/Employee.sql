25250f343ee81420c44b31d86cdd7ec340a6f722
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2019-03-18 13:31:01 +0700

    Add files via upload

--- SQL/Homewok_IT_School/Employee.sql
+++ SQL/Homewok_IT_School/Employee.sql
@@ -1,55 +1,56 @@
-#1
+#1 Исправлено
 
-SELECT e.name, d.name AS dep, chief.name, d.name AS chiefDepartment
+SELECT e.name, dep.name AS dep, chief.name, d.name AS chiefDepartment
 FROM employee AS e
-INNER JOIN employee AS chief
-	ON e.chief_id = chief.id
+LEFT JOIN employee AS chief
+	ON e.chief_id = chief.id 
+LEFT JOIN department AS dep
+ON e.department_id = dep.id
 LEFT JOIN department AS d
-	ON e.department_id = d.id
-INNER JOIN employee AS chiefDep
-	ON d.id = chiefDep.department_id;
+	ON chief.department_id = d.id;
 
-#2
+#2 Исправлено
 
 SELECT e.name, e.salary, chief.name AS chiefName, chief.salary AS chiefSalary
 FROM employee AS e
-INNER JOIN employee AS chief
+LEFT JOIN employee AS chief
 	ON e.chief_id = chief.id
-WHERE e.salary < chief.salary;
+WHERE e.salary > chief.salary;
 
-#3
+/*#3 Исправлено, но не знаю, как по стандарту сделать так, чтобы выводилось имя сотрудника, а группировка была по отделу*/
 
-SELECT e.name, d.name AS depName, MAX(e.salary)
-FROM employee AS e
-LEFT JOIN department AS d
-	ON e.department_id = d.id
-GROUP BY d.name;
+SELECT name, department_id, MAX(salary) 
+FROM employee
+GROUP BY department_id;
 
-#4
+#4 Перенесен COUNT из HAVING в SELECT
 
-SELECT department_id  FROM employee
+SELECT department_id, COUNT(*) AS emp FROM employee
 GROUP BY department_id
-HAVING COUNT(*) >= 3;
+HAVING emp >= 3;
 
-#5
+#5 Исправлено
 
 SELECT e.name, e.department_id FROM employee AS e
-INNER JOIN employee AS chief
+LEFT JOIN employee AS chief
 	ON e.chief_id = chief.id
-WHERE (e.chief_id <> e.id) AND (e.department_id <> chief.department_id);
+WHERE e.department_id <> chief.department_id;
 
-#6
+#6 Исправлено частично, но не знаю, как сделать, не применяя LIMIT
 
-SELECT d.name AS depName FROM department AS d
+SELECT d.name AS depName, SUM(e.salary) AS com_sum FROM department AS d
 LEFT JOIN employee AS e
 	ON d.id = e.department_id
 GROUP BY d.name
-ORDER BY SUM(e.salary) DESC;
+ORDER BY com_sum DESC
+LIMIT 1;
 
-#7
+#7 Не исправлено, пока не могу понять, как сделать без LIMIT
 
 SELECT name, salary FROM employee
 ORDER BY salary DESC
 LIMIT 2, 1;
+
+
 
 
