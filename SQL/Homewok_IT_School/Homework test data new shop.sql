e28fc554b02f5a988876470e024fb67d48a89761
Elina <48168023+ElinaGorniker@users.noreply.github.com>
2019-03-19 11:05:04 +0700

    Update and rename test data new shop.sql to Homework test data new shop.sql

--- SQL/Homewok_IT_School/Homework test data new shop.sql
+++ SQL/Homewok_IT_School/Homework test data new shop.sql
@@ -2,10 +2,10 @@ DELIMITER $$
 CREATE PROCEDURE test_data()
 BEGIN
 	DECLARE i INT DEFAULT 1;
-    WHILE i<=100 DO
+    WHILE i <= 100 DO
 		INSERT INTO category (name)
-        VALUES (CONCAT('category',i));
-        SET i=i+1;
+        VALUES (CONCAT('category', i));
+        SET i = i + 1;
 	END WHILE;
 END $$
 
@@ -17,8 +17,8 @@ BEGIN
 	DECLARE i INT DEFAULT 1;
     WHILE i<=500 DO
 		INSERT INTO product (name, price, categoryId)
-        VALUES (CONCAT('name',i), (SELECT FLOOR(1000 + (RAND() * 25000))), (SELECT FLOOR(1 + (RAND() * 102))));
-        SET i=i+1;
+        VALUES (CONCAT('name', i), (FLOOR(1000 + (RAND() * 25000))), (FLOOR(1 + (RAND() * 102))));
+        SET i = i + 1;
 	END WHILE;
 END $$
 
